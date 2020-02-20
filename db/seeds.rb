bed5b022b46a0be686df4944f8dd5b842e3d71d3
Ines Belghiti <ines.belghiti@gmail.com>
2020-02-20 13:45:46 -0500

    Added a cocktail to start with in the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,9 @@
 require 'json'
 require 'open-uri'
 
+
+# Ingredients
+
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 ingredients_serialized = open(url).read
 ingredients = JSON.parse(ingredients_serialized)["drinks"].map{|h| h["strIngredient1"]}
@@ -16,3 +19,36 @@ ingredients = JSON.parse(ingredients_serialized)["drinks"].map{|h| h["strIngredi
 ingredients.each do |ingredient|
   Ingredient.create(name: ingredient)
 end
+
+Ingredient.create(name: 'Ice')
+Ingredient.create(name: 'Mint')
+Ingredient.create(name: 'Club soda')
+
+
+# 1 cocktail to start with!
+
+mojito = Cocktail.create(name: 'Mojito')
+
+
+Dose.create(description: '20 leaves',
+            cocktail: mojito,
+            ingredient: Ingredient.where(name: 'Mint').first)
+
+Dose.create(description: '1/3 cup',
+            cocktail: mojito,
+            ingredient: Ingredient.where(name: 'Sugar').first)
+Dose.create(description: '4 cups',
+            cocktail: mojito,
+            ingredient: Ingredient.where(name: 'Club soda').first)
+Dose.create(description: '3/4 cup',
+            cocktail: mojito,
+            ingredient: Ingredient.where(name: 'Dark rum').first)
+Dose.create(description: '3/4 cup',
+            cocktail: mojito,
+            ingredient: Ingredient.where(name: 'Lime juice').first)
+Dose.create(description: '20 cubes',
+            cocktail: mojito,
+            ingredient: Ingredient.where(name: 'Ice').first)
+Dose.create(description: '1 (quartered)',
+            cocktail: mojito,
+            ingredient: Ingredient.where(name: 'Lime').first)
