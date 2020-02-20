518b1385c31c5e046cdec52e73a981ed8e56754a
Ines Belghiti <ines.belghiti@gmail.com>
2020-02-20 13:48:09 -0500

    Completed US: A user can add a new dose (ingredient/description pair) to an existing cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,34 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = set_cocktail
+  end
+
+  def create
+    dose = Dose.new(dose_description_and_ingredient)
+    cocktail = set_cocktail
+    dose.cocktail = cocktail
+    dose.save
+    redirect_to cocktail_path(cocktail)
+  end
+
+  def destroy
+    dose = set_dose
+    dose.destroy
+    redirect_to cocktail_path(get_cocktail)
+  end
+
+  private
+
+  def set_dose
+    Dose.find(params[:id])
+  end
+
+  def set_cocktail
+    Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_description_and_ingredient
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
