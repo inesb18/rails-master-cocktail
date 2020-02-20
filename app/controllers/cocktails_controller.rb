bda3558a6ed731e627430daeaeb5f99897dc7391
Ines Belghiti <ines.belghiti@gmail.com>
2020-02-20 12:54:35 -0500

    Completed the US: A user can create a new cocktail (no design)

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,7 +2,23 @@ class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
   end
+
   def show
     @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    cocktail = Cocktail.create(cocktail_name)
+    redirect_to cocktail_path(cocktail)
+  end
+
+  private
+
+  def cocktail_name
+    params.require(:cocktail).permit(:name)
   end
 end
